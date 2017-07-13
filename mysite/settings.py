515074faea4a360296a79d41fbb5ab1aa2a1eae9
Hellen <540676409@qq.com>
2017-07-13 21:17:16 +0800

    数据展示

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': 'test',
         'USER': 'test',
         'PASSWORD': 'as123456',
-        'HOST':'192.168.234.128',
+        'HOST':'192.168.1.103',
         'PORT':'3306',
     }
 }
