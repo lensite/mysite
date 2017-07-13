57c3778580e4f50a36273f4f8ac90f85de957522
lensite <baoleipaul@163.com>
2017-07-13 19:41:57 +0800

    数据展示

--- mysite/homepage.py
+++ mysite/homepage.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
-from django.http import HttpResponse
+
 from django.shortcuts import render
-from site1.models import usertable,hosttable
-from django.forms.models import model_to_dict
-from django.http import HttpResponseRedirect
-from django.views.decorators import csrf
+from site1.models import hosttable
+import datetime
 
 def home(request):
+    starttime = datetime.datetime.now()
+    endtime = datetime.datetime.now()
+    print(starttime)
+    print((endtime - starttime).seconds)
+
     result = hosttable.objects.values('hostname','hostip','hosthdd','hostmem','hostcpu')
     item_list = {}
     item_list['tr_list'] = result
