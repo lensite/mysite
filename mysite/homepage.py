c7cc5af3aeac57d1fa34868e07f17c023bf3f7b0
lensite <baoleipaul@163.com>
2017-07-17 19:06:05 +0800

    数据展示

--- mysite/homepage.py
+++ mysite/homepage.py
@@ -9,7 +9,14 @@ def home(request):
     starttime = datetime.datetime.now()
     #endtime = datetime.datetime.now()
     #print((endtime - starttime).seconds)
+
     result = hosttable.objects.values('hostname','hostip','hosthdd','hostmem','hostcpu','dateTime')
     item_list = {}
     item_list['tr_list'] = result
+    endtime = result[0]['dateTime'].strftime('%Y-%m-%d %H:%M:%S')
+    print(result[0]['dateTime'])
+    print(endtime)
+    print(starttime)
+    d1 = datetime.datetime.strptime(endtime,'%Y-%m-%d %H:%M:%S')
+    print((starttime-d1).seconds)
     return render(request, "index.html",item_list)
