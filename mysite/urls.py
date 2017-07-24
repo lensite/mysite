9986aabc330e20f2ce9cf7a817d0788694511950
lensite <baoleipaul@163.com>
2017-07-24 17:48:08 +0800

    1

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls import url
 from django.contrib import admin
-from . import view,homepage
+from . import view
 urlpatterns = [
     #url(r'^templates/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.STATIC_PATH}),
     url(r'^admin/', admin.site.urls),
