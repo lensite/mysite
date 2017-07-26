b4756ec8931c74e51ee79eaefff4f3c913296743
lensite <baoleipaul@163.com>
2017-07-26 13:34:43 +0800

    1

--- mysite/charts.py
+++ mysite/charts.py
@@ -1,11 +1,8 @@
-# # -*- coding: utf-8 -*-
-# from django.shortcuts import render
-# from site1.models import usertable,hosttable,hoststate
-# from django.http import HttpResponseRedirect
-# from . import Calculation
-# def host_state(request):
-#     host_info= hoststate.objects.filter(hostid='2017072401').values('dateTime','interrecv').order_by('dateTime')
-#     state_list = {}
-#     state_list['inter'] = host_info
-#     Calculation.Comparative_time()
-#     return render(request, "charts.html",state_list)
+# -*- coding: utf-8 -*-
+from django.shortcuts import render
+from site1.models import hoststate
+def host_state(request):
+    host_info= hoststate.objects.filter(hostid='2017072401').values('dateTime','interrecv').order_by('dateTime')
+    state_list = {}
+    state_list['inter'] = host_info
+    return render(request, "charts.html",state_list)
